f27be68367658aefd3c4c516f1a5a391d9d7e697
Muhammad Zain Khan <muhammadzainkhan131@gmail.com>
2025-05-26 05:55:47 +0500

    assigment 6 t0 9

---
+++ app.js
@@ -0,0 +1,68 @@
+// chapter # 6-9
+// Question#1
+
+  // document.write("<h3>Result</h3>");
+  // document.write("The value of a is:10 <br><br>")
+
+  // document.write("The value of ++a is:11 <br>")
+  // document.write("now tha value of a is:11 <br><br>")
+
+  // document.write("The value of a++ is:11 <br>")
+  // document.write("now tha value of a is:12 <br><br>")
+
+  // document.write("The value of --a is:11 <br>")
+  // document.write("now tha value of a is:11 <br><br>")
+
+  //  document.write("The value of a-- is:11 <br>")
+  // document.write("now tha value of a is:10 <br><br>")
+
+  // Question#2
+
+//   var a = 2 , b = 1
+
+//   var result =--a - --b + ++b + b--;
+//   console.log(result)
+
+//  document.write("a is " + "<b>" + a + "</b>" + "<br/>");
+//  document.write("b is " +"<b>" + b + "</b>" + "<br/>");
+//  document.write(" Method:  --a - --b + ++b + b--" + "<br/>");
+//  document.write(" Explanation:  1 - 0 + 1 + 1" + "<br/>");
+//  document.write("Result is " + "<b>" + result + "</b>" + "<br/>");
+
+// // Question#3
+
+// var user = prompt("Enter Your Name");
+// alert("Hello, " + user + "\n Welcome To Our Website!")
+
+// Question#5
+
+// var table =Number(prompt("You want to see table of:" ,5));
+// var num = 1;
+
+// document.write(table + " x " + num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+// document.write(table + " x " + ++num + " = " + (table*num) + "<br/>");
+
+// Question#6
+
+// var science=Number(prompt("How many marks did you get in science"));
+// var english=Number(prompt("How many marks did you get in english"));
+// var computer=Number(prompt("How many marks did you get in computer"));
+// var a = 100 , b = 100 , c= 100
+// var total_num = science + computer +english;
+// var total_marks =a+b+c;
+
+// console.log(total_num);
+// console.log(total_marks);
+
+// document.write("Science marks: "+ a +"/  Total number: "+ science +"/ percentage: " +((science/a) * 100)+"%<br><br>");
+// document.write("English marks: "+ a +"/  Total number: "+ english +"/ percentage: " +((english/a) * 100)+"%<br><br>");
+// document.write("Computer marks: "+ a +"/  Total number: "+ computer +"/ percentage: " +((computer/a) * 100)+"%<br><br>");
+// document.write("Total marks:" + (a+b+c) + "<br/>Total number:"+(science+english+computer)+"<br/> percentage: "+((total_num/total_marks) * 100)+"%<br>" );
